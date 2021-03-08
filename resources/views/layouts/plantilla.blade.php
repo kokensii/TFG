e6b46e1b5ebfb8da60d6d1b38a0a5eaee82d7052
unknown <jorgefavi96@gmail.com>
2021-03-08 18:29:44 +0100

    Añadida la nueva parte de autorización para hacer los logins y los registros

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width-device-width, initial-scale-1.0">
         <title>@yield('title')</title>
-        <link rel="stylesheet" href="/public/css/app.css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
 		<!-- favicon -->
 		<!-- estilos -->
 	</head>
