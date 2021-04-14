ae510984e3e6e8bc64dfde714458bd3efba9a3fa
unknown <jorgefavi96@gmail.com>
2021-04-14 15:56:16 +0200

    Nuevo formulario de creación de jornada

--- resources/views/creacion_contenido/create_jornada.blade.php
+++ resources/views/creacion_contenido/create_jornada.blade.php
@@ -3,12 +3,15 @@
 @section('title', 'Añadir jornada')
 
 @section('content')
-    <form action="{{route('jornada.store')}}" method="POST">
+    <form class="formulario" action="{{route('jornada.store')}}" method="POST">
         @csrf
-        <label>
-            Equipo Local 1:
-            <input type="text" name="id_equipo_local1" value="{{old('id_equipo_local1')}}">
-        </label>
+
+        <div class="form-group row">
+            <label for="id_equipo_local1" class="col-sm-2 col-form-label">Equipo Local 1</label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" id="id_equipo_local1" name="id_equipo_local1" value="{{old('id_equipo_local1')}}">
+            </div>
+        </div>
 
         @error('id_equipo_local1')
             <br>
@@ -17,10 +20,13 @@
         @enderror
 
         <br>
-        <label>
-            Equipo Visitante 1:
-            <input type="text" name="id_equipo_visitante1" value="{{old('id_equipo_visitante1')}}">
-        </label>
+
+        <div class="form-group row">
+            <label for="id_equipo_visitante1" class="col-sm-2 col-form-label">Equipo Visitante 1</label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" id="id_equipo_visitante1" name="id_equipo_visitante1" value="{{old('id_equipo_visitante1')}}">
+            </div>
+        </div>
 
         @error('id_equipo_visitante1')
             <br>
@@ -29,10 +35,13 @@
         @enderror
 
         <br>
-        <label>
-            Resultado 1:
-            <input type="text" name="resultado1" value="{{old('resultado1')}}">
-        </label>
+
+        <div class="form-group row">
+            <label for="resultado1" class="col-sm-2 col-form-label">Resultado 1</label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" id="resultado1" name="resultado1" value="{{old('resultado1')}}">
+            </div>
+        </div>
 
         @error('resultado1')
             <br>
@@ -41,10 +50,13 @@
         @enderror
 
         <br>
-        <label>
-            Equipo Local 2:
-            <input type="text" name="id_equipo_local2" value="{{old('id_equipo_local2')}}">
-        </label>
+
+        <div class="form-group row">
+            <label for="id_equipo_local2" class="col-sm-2 col-form-label">Equipo Local 2</label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" id="id_equipo_local2" name="id_equipo_local2" value="{{old('id_equipo_local2')}}">
+            </div>
+        </div>
 
         @error('id_equipo_local2')
             <br>
@@ -53,10 +65,13 @@
         @enderror
 
         <br>
-        <label>
-            Equipo Visitante 2:
-            <input type="text" name="id_equipo_visitante2" value="{{old('id_equipo_visitante2')}}">
-        </label>
+
+        <div class="form-group row">
+            <label for="id_equipo_visitante2" class="col-sm-2 col-form-label">Equipo Visitante 2</label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" id="id_equipo_visitante2" name="id_equipo_visitante2" value="{{old('id_equipo_visitante2')}}">
+            </div>
+        </div>
 
         @error('id_equipo_visitante2')
             <br>
@@ -65,10 +80,13 @@
         @enderror
 
         <br>
-        <label>
-            Resultado 2:
-            <input type="text" name="resultado2" value="{{old('resultado2')}}">
-        </label>
+
+        <div class="form-group row">
+            <label for="resultado2" class="col-sm-2 col-form-label">Resultado 2</label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" id="resultado2" name="resultado2" value="{{old('resultado2')}}">
+            </div>
+        </div>
 
         @error('resultado2')
             <br>
@@ -77,6 +95,6 @@
         @enderror
 
         <br>
-        <button type="submit">Enviar</button>
+        <button type="submit" class="btn btn-outline-primary">Enviar</button>
     </form>
 @endsection
