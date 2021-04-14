646bd218919568adf47eade23643c30902e96fd1
unknown <jorgefavi96@gmail.com>
2021-04-14 15:39:25 +0200

    Eliminado la clase content-main del formulario de creación de equipos dado que no es necesario

--- resources/views/creacion_contenido/create_team.blade.php
+++ resources/views/creacion_contenido/create_team.blade.php
@@ -3,54 +3,54 @@
 @section('title', 'Añadir equipo')
 
 @section('content')
-    <div class="content-main">
-        <form class="formulario" action="{{route('team.store')}}" method="POST">
-            @csrf
-            <div class="form-group row">
-                <label for="name" class="col-sm-1 col-form-label">Nombre</label>
-                <div class="col-sm-4">
-                    <input type="text" class="form-control" id="name" name="name" value="{{old('name')}}">
-                </div>
+    
+    <form class="formulario" action="{{route('team.store')}}" method="POST">
+        @csrf
+        <div class="form-group row">
+            <label for="name" class="col-sm-1 col-form-label">Nombre</label>
+            <div class="col-sm-4">
+                <input type="text" class="form-control" id="name" name="name" value="{{old('name')}}">
             </div>
+        </div>
 
-            @error('name')
-                <br>
-                <small>*{{$message}}</small>
-                <br>
-            @enderror
-
+        @error('name')
             <br>
+            <small>*{{$message}}</small>
+            <br>
+        @enderror
 
-            <div class="form-group row">
-                <label for="temporada" class="col-sm-1 col-form-label">Temporada</label>
-                <div class="col-sm-2">
-                    <input type="text" class="form-control" id="temporada" name="temporada" value="{{old('temporada')}}">
-                </div>
+        <br>
+
+        <div class="form-group row">
+            <label for="temporada" class="col-sm-1 col-form-label">Temporada</label>
+            <div class="col-sm-2">
+                <input type="text" class="form-control" id="temporada" name="temporada" value="{{old('temporada')}}">
             </div>
+        </div>
 
-            @error('temporada')
-                <br>
-                <small>*{{$message}}</small>
-                <br>
-            @enderror
-
+        @error('temporada')
             <br>
+            <small>*{{$message}}</small>
+            <br>
+        @enderror
 
-            <div class="form-group row">
-                <label for="urlImage" class="col-sm-1 col-form-label">Imagen</label>
-                <div class="col-sm-4">
-                    <input type="file" class="form-control" id="urlImage" name="urlImage" value="{{old('urlImage')}}">
-                </div>
+        <br>
+
+        <div class="form-group row">
+            <label for="urlImage" class="col-sm-1 col-form-label">Imagen</label>
+            <div class="col-sm-4">
+                <input type="file" class="form-control" id="urlImage" name="urlImage" value="{{old('urlImage')}}">
             </div>
+        </div>
 
-            @error('urlImagen')
-                <br>
-                <small>*{{$message}}</small>
-                <br>
-            @enderror
-
+        @error('urlImagen')
             <br>
-            <button type="submit" class="btn btn-outline-primary">Enviar</button>
-        </form>
-    </div>
+            <small>*{{$message}}</small>
+            <br>
+        @enderror
+
+        <br>
+         <button type="submit" class="btn btn-outline-primary">Enviar</button>
+    </form>
+    
 @endsection
