bff34a16f8e54ec2713e6e4aadc98a0ea394b168
unknown <jorgefavi96@gmail.com>
2021-04-14 15:29:03 +0200

    Cambio en el diseño de los formularios (de momento solo cambiado el de creación de equipos)

--- resources/views/creacion_contenido/create_team.blade.php
+++ resources/views/creacion_contenido/create_team.blade.php
@@ -3,44 +3,54 @@
 @section('title', 'Añadir equipo')
 
 @section('content')
-    <form action="{{route('team.store')}}" method="POST">
-        @csrf
-        <label>
-            Nombre:
-            <input type="text" name="name" value="{{old('name')}}">
-        </label>
+    <div class="content-main">
+        <form class="formulario" action="{{route('team.store')}}" method="POST">
+            @csrf
+            <div class="form-group row">
+                <label for="name" class="col-sm-1 col-form-label">Nombre</label>
+                <div class="col-sm-4">
+                    <input type="text" class="form-control" id="name" name="name" value="{{old('name')}}">
+                </div>
+            </div>
 
-        @error('name')
-            <br>
-            <small>*{{$message}}</small>
-            <br>
-        @enderror
+            @error('name')
+                <br>
+                <small>*{{$message}}</small>
+                <br>
+            @enderror
 
-        <br>
-        <label>
-            Temporada:
-            <input type="text" name="temporada" value="{{old('temporada')}}">
-        </label>
-
-        @error('temporada')
             <br>
-            <small>*{{$message}}</small>
-            <br>
-        @enderror
 
-        <br>
-        <label>
-            Imagen:
-            <input type="file" name="urlImage" value="{{old('urlmage')}}">
-        </label>
+            <div class="form-group row">
+                <label for="temporada" class="col-sm-1 col-form-label">Temporada</label>
+                <div class="col-sm-2">
+                    <input type="text" class="form-control" id="temporada" name="temporada" value="{{old('temporada')}}">
+                </div>
+            </div>
 
-        @error('urlImagen')
-            <br>
-            <small>*{{$message}}</small>
-            <br>
-        @enderror
+            @error('temporada')
+                <br>
+                <small>*{{$message}}</small>
+                <br>
+            @enderror
 
-        <br>
-        <button type="submit">Enviar</button>
-    </form>
+            <br>
+
+            <div class="form-group row">
+                <label for="urlImage" class="col-sm-1 col-form-label">Imagen</label>
+                <div class="col-sm-4">
+                    <input type="file" class="form-control" id="urlImage" name="urlImage" value="{{old('urlImage')}}">
+                </div>
+            </div>
+
+            @error('urlImagen')
+                <br>
+                <small>*{{$message}}</small>
+                <br>
+            @enderror
+
+            <br>
+            <button type="submit" class="btn btn-outline-primary">Enviar</button>
+        </form>
+    </div>
 @endsection
