da8281cf0f2529352894b3ca2eb445bf787486a2
kokensi <jorgefavi96@gmail.com>
2021-07-16 12:16:29 +0200

    Cambio en el estilo de los formularios

--- resources/views/creacion_contenido/create_cromo.blade.php
+++ resources/views/creacion_contenido/create_cromo.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Añadir cromo')
 
 @section('content')
-    <form class="formulario" action="{{route('cromo.store')}}" method="POST">
+    <!--<form class="formulario" action="{{route('cromo.store')}}" method="POST">
         @csrf
 
         <div class="form-group row">
@@ -70,5 +70,64 @@
 
         <br>
         <button type="submit" class="btn btn-outline-primary">Enviar</button>
-    </form>
+    </form>-->
+    <div class="form">
+        <div class="title">Añadir Cromo</div>
+        <form action="{{route('cromo.store')}}" method="POST">
+            @csrf
+
+            <div class="form-details">
+                <div class="input-box">
+                    <span class="details">Nombre</span>
+                    <input type="text" id="name" name="name" value="{{old('name')}}" required>
+                </div>
+
+                @error('name')
+                    <br>
+                    <small>*{{$message}}</small>
+                    <br>
+                @enderror
+
+                <div class="input-box">
+                    <span class="details">Temporada</span>
+                    <input type="text" id="temporada" name="temporada" value="{{old('temporada')}}" required>
+                </div>
+
+                @error('temporada')
+                    <br>
+                    <small>*{{$message}}</small>
+                    <br>
+                @enderror
+
+                <div class="input-box">
+                    <span class="details">Imagen</span>
+                    <input type="file" id="urlImage" name="urlImage" value="{{old('urlImage')}}">
+                </div>
+
+                @error('urlImage')
+                    <br>
+                    <small>*{{$message}}</small>
+                    <br>
+                @enderror
+
+                <div class="input-box">
+                    <span class="details">Equipo</span>
+                    <select name="id_equipo" required>
+                        @foreach($equipos as $equipo)
+                            <option value="{{$equipo->id}}">{{$equipo->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                @error('id_equipo')
+                    <br>
+                    <small>*{{$message}}</small>
+                    <br>
+                @enderror
+            </div>
+            <div class="button">
+                <input type="submit" value="Enviar">
+            </div>
+        </form>
+    </div>
 @endsection
--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,8 +5,9 @@
 		<meta name="viewport" content="width-device-width, initial-scale-1.0">
         <title>@yield('title')</title>
 		
-		<link href="{{ asset('css/prueba.css') }}" rel="stylesheet" type="text/css">
-		<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="{{ asset('css/estiloForm.css') }}">
+		<!--<link href="{{ asset('css/prueba.css') }}" rel="stylesheet" type="text/css">
+		<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">-->
 		
 		<!-- favicon -->
 		<!-- estilos -->
