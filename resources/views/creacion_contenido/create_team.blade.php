9cc68f3f98d680bc144bb4323a8c889be0b214a1
kokensi <jorgefavi96@gmail.com>
2021-07-16 13:37:37 +0200

    Cambio en el formulario de añadir equipo (provisional)

--- resources/views/creacion_contenido/create_team.blade.php
+++ resources/views/creacion_contenido/create_team.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Añadir equipo')
 
 @section('content')
-    
+    <!--
     <form class="formulario" action="{{route('team.store')}}" method="POST">
         @csrf
         <div class="form-group row">
@@ -51,6 +51,52 @@
 
         <br>
          <button type="submit" class="btn btn-outline-primary">Enviar</button>
-    </form>
-    
+    </form>-->
+
+    <div class="wrapper">
+        <div class="form">
+            <div class="title">Añadir Equipo</div>
+            <form action="{{route('jornada.store')}}" method="POST">
+                @csrf
+                <div class="form-details">
+                    <div class="input-box">
+                        <span class="details">Nombre</span>
+                        <input type="text" id="name" name="name" value="{{old('name')}}" required>
+                    </div>
+
+                    @error('name')
+                        <br>
+                        <small>*{{$message}}</small>
+                        <br>
+                    @enderror
+
+                    <div class="input-box">
+                        <span class="details">Temporada</span>
+                        <input type="text" id="temporada" name="temporada" value="{{old('temporada')}}" required>
+                    </div>
+
+                    @error('temporada')
+                        <br>
+                        <small>*{{$message}}</small>
+                        <br>
+                    @enderror
+
+                    <div class="input-box">
+                        <span class="details">Imagen</span>
+                        <input type="file" id="urlImage" name="urlImage" value="{{old('urlImage')}}">
+                    </div>
+
+                    @error('urlImage')
+                        <br>
+                        <small>*{{$meessage}}</small>
+                        <br>
+                    @enderror
+                </div>
+
+                <div class="button">
+                    <input type="submit" value="Enviar">
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
