c68fd58929e48daa99128c3aafaca5fb9f075d50
kokensi <jorgefavi96@gmail.com>
2021-07-30 16:36:28 +0200

    Cambio en los formularios de edit de preguntas y equipos

--- resources/views/creacion_contenido/edit_team.blade.php
+++ resources/views/creacion_contenido/edit_team.blade.php
@@ -3,54 +3,50 @@
 @section('title', 'Editar equipo')
 
 @section('content')
-    <form class="formulario" action="{{route('team.update', $team)}}" method="post">
-        @csrf
-        @method('put')
+    <div class="form">
+        <div class="title">Editar pregunta</div>
+        <form action="{{route('team.update', $team)}}" method="POST">
+            @csrf
+            @method('put')
 
-        <div class="form-group row">
-            <label for="name" class="col-sm-1 col-form-label">Nombre</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" id="name" name="name" value="{{old('name', $team->name)}}">
+            <div class="form-details">
+                <div class="input-box">
+                    <span class="details">Nombre</span>
+                    <input type="text" name="name" id="name" value="{{old('team', $team->name)}}">
+                </div>
+
+                @error('name')
+                    <br>
+                    <small>*{{$message}}</small>
+                    <br>
+                @enderror
+
+                <div class="input-box">
+                    <span class="details">Temporada</span>
+                    <input type="text" name="temporada" id="temporada" value="{{old('temporada', $team->temporada)}}">
+                </div>
+
+                @error('temporada')
+                    <br>
+                    <small>*{{$message}}</small>
+                    <br>
+                @enderror
+
+                <div class="input-box">
+                    <span class="details">Imagen</span>
+                    <input type="file" name="urlImage" id="urlImage" value="{{old('urlImage', $team->urlImage)}}">
+                </div>
+
+                @error('urlImage')
+                    <br>
+                    <small>*{{$message}}</small>
+                    <br>
+                @enderror
+
+                <div class="button">
+                    <input type="submit" value="Enviar">
+                </div>
             </div>
-        </div>
-
-        @error('name')
-            <br>
-            <small>*{{$message}}</small>
-            <br>
-        @enderror
-
-        <br>
-
-        <div class="form-group row">
-            <label for="temporada" class="col-sm-1 col-form-label">Temporada</label>
-            <div class="col-sm-4">
-                <input type="text" class="form-control" id="temporada" name="temporada" value="{{old('temporada', $team->temporada)}}">
-            </div>
-        </div>
-
-        @error('temporada')
-            <br>
-            <small>*{{$message}}</small>
-            <br>
-        @enderror
-
-        <br>
-
-        <div class="form-group row">
-            <label for="urlImage" class="col-sm-1 col-form-label">Imagen</label>
-            <div class="col-sm-4">
-                <input type="file" class="form-control" id="urlImage" name="urlImage" value="{{old('urlImage', $team->urlImage)}}">
-            </div>
-        </div>
-
-        @error('urlImage')
-            <br>
-            <small>*{{$message}}</small>
-            <br>
-        @enderror
-
-        <br>
-        <button type="submit" class="btn btn-outline-primary">Editar</button>
-    </form>
+        </form>
+    </div>
 @endsection
