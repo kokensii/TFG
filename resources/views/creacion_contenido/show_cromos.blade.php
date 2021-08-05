4167217f252b8ca5c317edaa54f4c1a6f8d9250e
kokensi <jorgefavi96@gmail.com>
2021-08-05 13:12:19 +0200

    Añadida la función de eliminar en la parte de cromos

--- resources/views/creacion_contenido/show_cromos.blade.php
+++ resources/views/creacion_contenido/show_cromos.blade.php
@@ -1,11 +1,24 @@
-@extends('layouts.listados')
+@extends('layouts.plantilla')
 
 @section('title', 'Listado cromos')
 
 @section('content')
     <div class="wrapper">
-        @foreach($cromos as $cromo)
-            <li>{{$cromo->name}}</li>
-        @endforeach
+        <div class="form-listado">
+            @foreach($cromos as $cromo)
+                <div class="form-all">
+                    <li>{{$cromo->name}}</li>
+                    <form action="{{ route('cromo.destroy', $cromo) }}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="Eliminar">
+                    </form>
+                    <form action="{{ route('cromo.edit', $cromo) }}" method="GET">
+                        @csrf
+                        <input type="submit" value="Editar">
+                    </form>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
