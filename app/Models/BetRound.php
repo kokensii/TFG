6a56f385927b2b51f73b2013c0ae40879b6740fb
kokensi <jorgefavi96@gmail.com>
2021-11-30 17:57:50 +0100

    Pequeños cambios en el modelo de BetRound

--- app/Models/BetRound.php
+++ app/Models/BetRound.php
@@ -8,9 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class BetRound extends Model
 {
     use HasFactory;
+    public $timestamp = false;
     public $table = "bet_round";
     protected $fillable = [
-        'id', 'id_home_team', 'id_away_team', 'result_home', 'result_away', 'id_rounds',
+        'id', 'id_home_team', 'id_away_team', 'result', 'id_home_team_2', 'id_away_team_2', 
+        'result_2', 'id_jornada', 'end', 'updated_at', 'created_at',
     ];
     function equipo(){
         return $this->hasOne('Team','id_home_team');
