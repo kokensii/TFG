9ebe6f9ed463a612a142f5ac35edf32819fefd53
Miguel <migvega@ucm.es>
2022-07-14 19:37:52 +0200

    Update cromos.blade.php

--- resources/views/interaccion_usuarios/cromos.blade.php
+++ resources/views/interaccion_usuarios/cromos.blade.php
@@ -3,18 +3,19 @@
 @section('title', 'Listado de cromos')
 
 @section('content')
-    <div class="">
+    <div class="wrapper">
         @foreach($teams as $team)
             <h1>{{ $team->name }} (0/4)</h1>
             @foreach($jugadores as $jugador)
                 @if($jugador->id_equipo == $team->id)
                     {{ $jugador->nombre }}
+                    {{ $jugador->id }}
+                    {{ $jugador->nombre }}
+                    @php($variable = "../../../" . $jugador->url_imagen)
+                    <img src = {{$variable}} width=200>
+        
                 @endif
             @endforeach
         @endforeach
-        {{-- @foreach($jugadores as $player)
-            {{ $player->id }}
-            {{ $player->nombre }}
-        @endforeach --}}
     </div>
 @endsection
